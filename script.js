828aafcdad2234adaf5cd690dac6e87d736cf7c6
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2024-12-28 20:18:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+    const fname=document.getElementById("fname").value;
+	const lname=document.getElementById("lname").value; 
+	const alertMessage=`${fname} ${lname}`;
+	alert(alertMessage);
 }
